e4c29375088dcc29767444b1544b58ec12b1f564
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 23:04:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,6 +52,10 @@ end
 
 def add_s(array)
   array.collect.with_index do |word, index|
-    index == 1 ? word : word + s
+    if index == 1
+      word
+    else
+      word + s
+    end
   end
 end
