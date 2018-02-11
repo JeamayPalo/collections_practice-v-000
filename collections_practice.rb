1587884d13d6e837b902fef04ae7d6c0e8acb295
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-11 22:53:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,6 +17,7 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array[1], array[2] = array [2], array[1]
-  array
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
 end
